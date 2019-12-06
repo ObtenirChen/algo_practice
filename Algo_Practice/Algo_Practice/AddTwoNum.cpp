05e694b6b5afd0deb538b031dc667882a9451347
chenyouhui <chenyouhui@yy.com>
2019-12-06 22:24:41 +0800

    C:两数之和

--- Algo_Practice/Algo_Practice/AddTwoNum.cpp
+++ Algo_Practice/Algo_Practice/AddTwoNum.cpp
@@ -7,3 +7,56 @@
 //
 
 #include <stdio.h>
+#include "ListNode.cpp"
+
+class AddTwoNum {
+    
+public:
+    
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode *head = new ListNode(0);
+        ListNode *ptr = head;
+        int add = 0;
+        int b = 0;
+        while (l1 != NULL && l2 != NULL) {
+            int a = l1->val + l2->val + add;
+            b = a % 10;
+            add = a / 10;
+            ListNode *temp = new ListNode(b);
+            ptr->next = temp;
+            ptr = ptr->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        
+        while (l1 != NULL) {
+            int a = l1->val + add;
+            b = a % 10;
+            add = a / 10;
+            ListNode *temp = new ListNode(b);
+            ptr->next = temp;
+            ptr = ptr->next;
+            l1 = l1->next;
+        }
+        
+        while (l2 != NULL) {
+            int a = l2->val + add;
+            b = a % 10;
+            add = a / 10;
+            ListNode *temp = new ListNode(b);
+            ptr->next = temp;
+            ptr = ptr->next;
+            l2 = l2->next;
+        }
+        
+        if (add != 0) {
+            ListNode *temp = new ListNode(add);
+            ptr->next = temp;
+            ptr = ptr->next;
+        }
+        
+        return head->next;
+    }
+    
+};
